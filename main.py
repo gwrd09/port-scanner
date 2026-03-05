97586be85d11d52d3a52798617d8487ffa5aab8e
George Ward <gwardgaming@gmail.com>
2026-03-05 10:20:51 +0000

    Added database support for SQlite allowing information to be stored about each port from every scan

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import socket
-
-TARGET = "scanme.nmap.org"
+import sqlite3
+import datetime
 
 def scan_port(hostname, port):
     address = socket.gethostbyname(hostname)
     s = socket.socket()
-    s.settimeout(0.5)
+    s.settimeout(1)
     port_status = s.connect_ex((address, port))
     if not port_status:
         print(f"Port {port} | Status: OPEN")
+        port_status = "OPEN"
     else:
         print(f"Port {port} | Status: CLOSED")
+        port_status = "CLOSED"
+    time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    con.execute("INSERT INTO scanner (ip, port, status, date) VALUES (?, ?, ?, ?)", (address, port, port_status, time))
+
+TARGET = "scanme.nmap.org"
+
+con = sqlite3.connect("scanner.db")
+con.execute("""
+    CREATE TABLE IF NOT EXISTS scanner(
+        id INTEGER PRIMARY KEY,
+        ip TEXT, 
+        port INTEGER, 
+        status TEXT, 
+        date TEXT
+    );
+""")
 
 for port in range(1, 101):
     scan_port(TARGET, port)
+con.commit()
