0b3472b9f4edf8ba161c3b5f503d69333fe3b0ff
George Ward <gwardgaming@gmail.com>
2026-03-09 21:20:59 +0000

    No longer commiting to database inside scan_port function instead it has its own function

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import socket
 import sqlite3
 import datetime
 
+port_data = []
+
 def scan_port(hostname, port):
     address = socket.gethostbyname(hostname)
     s = socket.socket()
-    s.settimeout(1)
+    s.settimeout(0.5)
     port_status = s.connect_ex((address, port))
     if not port_status:
         print(f"Port {port} | Status: OPEN")
@@ -14,9 +16,14 @@ def scan_port(hostname, port):
         print(f"Port {port} | Status: CLOSED")
         port_status = "CLOSED"
     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-    con.execute("INSERT INTO scanner (ip, port, status, date) VALUES (?, ?, ?, ?)", (address, port, port_status, time))
+    port_data.append([address, port, port_status, time])
 
-TARGET = "scanme.nmap.org"
+def upload_data(data_dict):
+    global port_data
+    for record in data_dict:
+        con.execute("INSERT INTO scanner (ip, port, status, date) VALUES (?, ?, ?, ?)", (record[0], record[1], record[2], record[3]))
+    con.commit()
+    port_data = []
 
 con = sqlite3.connect("scanner.db")
 con.execute("""
@@ -29,6 +36,8 @@ con.execute("""
     );
 """)
 
+TARGET = "scanme.nmap.org"
+
 for port in range(1, 101):
     scan_port(TARGET, port)
-con.commit()
+upload_data(port_data)
